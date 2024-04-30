daf1c105a872785673372439edf3864142b100ac
Linsen Loots <linsen@10x.co.za>
2024-04-30 14:53:43 +0200

    set up simple example project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="sentry-example">Sentry Example Page</a></p>
---
+++ src/routes/sentry-example/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+</script>
+
+<p>Separate example page</p>
+
+<a href="/">Go Back</a>
+
+<style>
+</style>
